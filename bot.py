3f83cee52371b7842afd276c47f8cea172663bd2
Men Zomboy <nick.markov2.0@gmail.com>
2025-12-09 13:18:40 +0200

    Add Procfile for Render deployment

--- bot.py
+++ bot.py
@@ -2,24 +2,46 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 
+from aiohttp import web
 from config import TOKEN
 from app.handlers import router
 from app.database.models import async_main
 
 
+async def start_web_server():
+    async def handle(request):
+        return web.Response(text="Bot is running!")
+
+    app = web.Application()
+    app.add_routes([web.get("/", handle)])
+    runner = web.AppRunner(app)
+
+    await runner.setup()
+
+    # Render передає PORT через env
+    import os
+    port = int(os.environ.get("PORT", 10000))
+
+    site = web.TCPSite(runner, "0.0.0.0", port)
+    await site.start()
+    print(f"Fake web server started on port {port}")
+
+
 async def main():
     logging.basicConfig(level=logging.INFO)
 
-    # Створюємо таблиці (users) з нуля
     await async_main()
 
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
-
-    # Підключаємо router!!!
     dp.include_router(router)
 
-    await dp.start_polling(bot)
+    # Запускаємо вебсервер + polling паралельно
+    await asyncio.gather(
+        start_web_server(),
+        dp.start_polling(bot)
+    )
+
 
 
 if __name__ == "__main__":
